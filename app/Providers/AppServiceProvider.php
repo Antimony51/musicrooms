74b5c787ff1963a2509c2e0d966b37a3896fa3ae
Michael Dickey <mdickey32@gmail.com>
2016-11-07 14:48:33 -0500

    removed phpstorm stuff

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -40,8 +40,5 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        if($this->app->environment() !== 'production') {
-            $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
-        }
     }
 }
